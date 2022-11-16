da75c4dc65a1a0348e336ee0b0209a93c4f5b657
Đorđe <the2922000@gmail.com>
2022-11-16 22:48:25 +0100

    Validate update form fields

--- ISA_FRONTEND/src/app/modules/blood-bank-center/edit-blood-bank/edit-blood-bank.component.ts
+++ ISA_FRONTEND/src/app/modules/blood-bank-center/edit-blood-bank/edit-blood-bank.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BloodBank } from 'src/app/model/bloodBank';
 import { BloodBankService } from 'src/app/services/blood-bank.service';
+import { EMPTY, catchError } from 'rxjs';
 
 @Component({
   selector: 'app-edit-blood-bank',
@@ -12,6 +13,7 @@ export class EditBloodBankComponent implements OnInit {
 
   id : string | null | undefined;
   bloodBank: BloodBank = new BloodBank;
+  greska?: String
   
   constructor(private route: ActivatedRoute, private router: Router, private bloodBankService:BloodBankService) { }
 
@@ -27,10 +29,27 @@ export class EditBloodBankComponent implements OnInit {
   }
 
   public updateBloodBank(): void {
-    this.bloodBankService.updateBloodBank(this.bloodBank).subscribe(res => {
-      this.bloodBank = res;
-      this.router.navigate(['/myBloodBank', this.bloodBank.id]);
-    })
+    if(this.inputOK()){
+      this.bloodBankService.updateBloodBank(this.bloodBank).pipe(
+        catchError(() => {
+          this.greska = "Korisničko ime zauzeto"
+          return EMPTY;
+        })
+      ).subscribe(res => {
+        this.bloodBank = res;
+        this.router.navigate(['/myBloodBank', this.bloodBank.id]);
+      })
+    }
+  }
+
+  inputOK(): boolean{
+    if(this.bloodBank.name == "" || this.bloodBank.description == ""
+      || this.bloodBank.address.street == "" ||this.bloodBank.address.streetNum == ""
+      ||this.bloodBank.address.city == "" || this.bloodBank.address.country == ""){
+        this.greska = "Popunite sva polja"
+        return false;
+    }
+    return true;
   }
 
 }
--- ISA_FRONTEND/src/app/modules/user-profiles/edit-medicine-staff-profile/edit-medicine-staff-profile.component.ts
+++ ISA_FRONTEND/src/app/modules/user-profiles/edit-medicine-staff-profile/edit-medicine-staff-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MedicineStaff } from 'src/app/model/medicineStaff';
 import { UserService } from 'src/app/services/user.service';
+import { EMPTY, catchError } from 'rxjs';
 
 @Component({
   selector: 'app-edit-medicine-staff-profile',
@@ -12,6 +13,7 @@ export class EditMedicineStaffProfileComponent implements OnInit {
 
   id?:string | null;
   medicineStaff : MedicineStaff = new MedicineStaff;
+  greska?: string
   constructor(private route: ActivatedRoute, private router: Router, private userService: UserService) { }
 
   ngOnInit(): void {
@@ -25,9 +27,28 @@ export class EditMedicineStaffProfileComponent implements OnInit {
     })
   }
   public updateMedicineStaff(): void {
-    this.userService.updateMedicineStaff(this.medicineStaff).subscribe(res => {
-      this.medicineStaff = res;
-      this.router.navigate(['/medicineStaffProfile', this.medicineStaff.id]);
-    })
+    if(this.inputOK()){
+      this.userService.updateMedicineStaff(this.medicineStaff).pipe(
+        catchError(() => {
+          this.greska = "Korisničko ime zauzeto"
+          return EMPTY;
+        })
+      )
+      .subscribe(res => {
+        this.medicineStaff = res;
+        this.router.navigate(['/medicineStaffProfile', this.medicineStaff.id]);
+      })
+    }
+  }
+
+  inputOK(): boolean{
+    if(this.medicineStaff.name == "" || this.medicineStaff.surname == "" || this.medicineStaff.username == ""
+     || this.medicineStaff.password == "" || this.medicineStaff.jmbg == "" || this.medicineStaff.mail == "" 
+     || this.medicineStaff.phoneNumber == "" || this.medicineStaff.address.street == "" ||this.medicineStaff.address.streetNum == ""
+     ||this.medicineStaff.address.city == "" || this.medicineStaff.address.country == ""){
+       this.greska = "Popunite sva polja"
+       return false;
+    }
+    return true;
   }
 }
