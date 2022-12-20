e3e676014067fd9465cc55b72b3eb67b76aa440c
Petar Popovic <93181074+Petarpop202@users.noreply.github.com>
2022-12-20 18:24:52 +0100

    fix

--- ISA_FRONTEND/src/app/modules/blood-bank-center/blood-bank-center.module.ts
+++ ISA_FRONTEND/src/app/modules/blood-bank-center/blood-bank-center.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BloodCenterComponent } from './blood-center/blood-center.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomepageModule } from '../homepage/homepage.module';
-import { EditBloodBankComponent } from './edit-blood-bank/edit-blood-bank.component';
 import { FormsModule } from '@angular/forms';
-import { NewBloodBankComponent } from './new-blood-bank/new-blood-bank.component';
 import { AddBloodDonationAppointmentComponent } from './add-blood-donation-appointment/add-blood-donation-appointment.component';
-
-
 
 
 @NgModule({
   declarations: [
-    BloodCenterComponent,
-    EditBloodBankComponent,
-    NewBloodBankComponent,
     AddBloodDonationAppointmentComponent
 
   ],
